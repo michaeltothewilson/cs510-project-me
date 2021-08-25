8842d4030186e541d93166deaa880015de92596d
michaeltothewilson <56896666+michaeltothewilson@users.noreply.github.com>
2021-08-25 16:08:28 -0700

    Nav Bar Active Link Function all good to go!

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 
+    // Go to the about section from the home page
     $("#about-button").click(function(){
         $('html, body').animate({
             scrollTop:$("#about").offset().top},500)
         })
+
+    // Control the active link in the navbar
+    $(".navbar .nav-link").click(function(){
+        $(".navbar").find(".active").removeClass("active")
+        $(this).addClass("active")
+    })
 })
